be6af6baf8340db642dec0163773aff32e445e4e
Paweł Kisielewicz <pkisiel@gmail.com>
2025-05-06 09:13:47 +0000

    New function menu

--- main.cpp
+++ main.cpp
@@ -8,15 +8,21 @@ void my_print(const string &text)
     cout << text << endl;
 }
 
+void menu(string &text)
+{
+    my_print("Podaj tekst");
+    getline(cin, text);
+}
+
 int main()
 {
     string text;
 
     my_print("Ala ma kota");
-    my_print("Podaj tekst");
-    getline(cin ,text);
+    menu(text);
     string newText = "Nowy tekst = " + text;
     my_print(newText);
 
     return 0;
 }
+
