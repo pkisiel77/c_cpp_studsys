90c0941097269ba02e865c4406a12635b1d2ca2a
Paweł Kisielewicz <pkisiel@gmail.com>
2025-06-10 15:02:19 +0000

    Refactoring

--- files/MyFileClass/analyzefile.cpp
+++ files/MyFileClass/analyzefile.cpp
@@ -65,14 +65,16 @@ void AnalyzedFile::showStats()
     // liczba bajtów pliku
 }
 
-std::vector<std::string> AnalyzedFile::split(const std::string& str, char delimiter) {
+std::vector<std::string> AnalyzedFile::split(const std::string &str, char delimiter)
+{
     std::vector<std::string> tokens;
     std::stringstream ss(str);
     std::string token;
-    
-    while (std::getline(ss, token, delimiter)) {
+
+    while (std::getline(ss, token, delimiter))
+    {
         tokens.push_back(token);
     }
-    
+
     return tokens;
 }
