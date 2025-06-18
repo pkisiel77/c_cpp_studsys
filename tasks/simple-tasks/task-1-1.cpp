cbf1e2c15d73e76c43a4883f007487d09cae0824
Paweł Kisielewicz <pkisiel@gmail.com>
2025-06-18 06:29:45 +0000

    namspace

--- tasks/simple-tasks/task-1-1.cpp
+++ tasks/simple-tasks/task-1-1.cpp
@@ -2,44 +2,59 @@
 #include <vector>
 #include <numeric>
 
-class Statystyka
+namespace _my_namespace
 {
-private:
-    std::vector<float> _data;
-
-public:
-    void setOneData(float d)
+    class Statystyka
     {
-        _data.push_back(d);
-    }
+    private:
+        std::vector<float> _data;
 
-    // średnią,
-    float getMean()
+    public:
+        void setOneData(float d)
+        {
+            _data.push_back(d);
+        }
+
+        // średnią,
+        float getMean()
+        {
+            if (_data.empty())
+                throw std::runtime_error("Cannot calculate mean of an empty dataset.");
+
+            auto sum = std::accumulate(_data.begin(), _data.end(), 0,
+                                       [](int a, int b)
+                                       {
+                                           return a + b;
+                                       });
+
+            // float sum = std::accumulate(numbers.begin(), numbers.end(), 0);
+
+            return static_cast<float>(sum) / _data.size();
+        }
+
+        // odchylenie standardowe,
+        float getStandardDeviation();
+
+        // medianę.
+        float getMedian();
+    };
+}
+
+namespace _ala_ma_kota
+{
+    void my_fun()
     {
-        if (_data.empty())
-            throw std::runtime_error("Cannot calculate mean of an empty dataset.");
 
-        auto sum = std::accumulate(_data.begin(), _data.end(), 0,
-                                   [](int a, int b)
-                                   {
-                                       return a + b;
-                                   });
-
-        // float sum = std::accumulate(numbers.begin(), numbers.end(), 0);
-        
-        return static_cast<float>(sum) / _data.size();
     }
+}
 
-    // odchylenie standardowe,
-    float getStandardDeviation();
-
-    // medianę.
-    float getMedian();
-};
+using namespace _my_namespace;
 
 int main()
 {
-    Statystyka s;
+    _my_namespace::Statystyka s;
+
+    _ala_ma_kota::my_fun();
 
     // 1. Get data
     float data = 1.2;
